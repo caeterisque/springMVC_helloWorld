b5b12bf30f891c5843a2ecbac520a522978c099c
caeterisque <sigaeva.katyusha@mail.ru>
2018-11-19 22:28:45 +0400

    22

--- src/main/java/com/company/User.java
+++ src/main/java/com/company/User.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
-
+///
 public class User {
     @NotNull
     @Size(min=2, max=30)
